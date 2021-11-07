1d1508daa3bcdb395a062f10571ec90a548ac3d5
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-11-07 08:55:35 -0800

    add my example shell.nix

---
+++ shell.nix
@@ -0,0 +1,27 @@
+# Example of a custom Jupyter server with OrthoLang 0.9.5,
+# as well as Python + R with custom packages
+# Usage: nix-shell --command 'jupyter lab'
+
+let
+  jupyter = import ./default.nix {};
+
+  python = jupyter.kernels.iPythonWith {
+    name = "shell.nix";
+    packages = p: with p; [ numpy ];
+  };
+
+  r = jupyter.kernels.iRWith {
+    name = "shell.nix";
+    packages = p: with p; [ p.tidyverse ];
+  };
+
+  # you can add a name, but there aren't packages like in python or R
+  ortholang = jupyter.kernels.ortholangKernel {
+    name = "pinned 0.9.5";
+  };
+
+  jupyterEnvironment = jupyter.jupyterlabWith {
+    kernels = [ python r ortholang ];
+  };
+in
+  jupyterEnvironment.env
